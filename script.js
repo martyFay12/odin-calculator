915d5d3bd42312afcf4b941f3077614d80e9dc17
Marty Fay <marty.r.fay@gmail.com>
2023-05-19 14:15:15 -0300

    Create recordOperation function

--- script.js
+++ script.js
@@ -1,12 +1,31 @@
 let operandFlag = true;
 let operand1 = "";
 let operand2 = "";
-let operation = "";
+let operationSaved = "";
 
 const digits = document.querySelectorAll(".digit");
 digits.forEach((digit) => digit.addEventListener("click", recordDigit));
+
+const operations = document.querySelectorAll(".operation");
+operations.forEach((operation) =>
+  operation.addEventListener("click", recordOperation)
+);
 
 function recordDigit() {
   const digit = this.textContent;
   operandFlag ? operand1 + digit : operand2 + digit;
 }
+
+function recordOperation() {
+  if (operationSaved) {
+    // already an operation saved
+    doMath();
+  }
+  operationSaved = this.textContent;
+}
+
+// function doMath(){
+//     switch(operationSaved){
+//         case('=')
+//     }
+// }
