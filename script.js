975e80b28d16fbac1e766dfd4be41c90dbc4465c
Marty Fay <marty.r.fay@gmail.com>
2023-05-19 15:43:09 -0300

    Add functionality to plus/minus button

--- script.js
+++ script.js
@@ -3,19 +3,42 @@ let operand1 = "";
 let operand2 = "";
 let operationSaved = "";
 
+// SELECT BUTTONS
 const digits = document.querySelectorAll(".digit");
-digits.forEach((digit) => digit.addEventListener("click", recordDigit));
-
 const operations = document.querySelectorAll(".operation");
+const decimalButton = document.querySelector(".decimal-digit");
+const equalsButton = document.querySelector(".equals");
+const clearButton = document.querySelector(".clear");
+const plusMinusButton = document.querySelector(".plus-minus");
+
+// ADD EVENT LISTENERS
+digits.forEach((digit) => digit.addEventListener("click", recordDigit));
 operations.forEach((operation) =>
   operation.addEventListener("click", recordOperation)
 );
-const decimalButton = document.querySelector(".decimal-digit");
-const equalsButton = document.querySelector(".equals");
 equalsButton.addEventListener("click", equalsPress);
 reset();
-const clearButton = document.querySelector(".clear");
 clearButton.addEventListener("click", reset);
+plusMinusButton.addEventListener("click", plusMinus);
+
+function plusMinus() {
+  if (operandFlag) {
+    if (operand1[0] === "-") {
+      operand1 = operand1.slice(1);
+    } else {
+      operand1 = "-" + operand1;
+    }
+  } else {
+    if (operand2[0] === "-") {
+      operand2 = operand2.slice(1);
+    } else {
+      operand2 = "-" + operand2;
+    }
+  }
+  console.log(
+    `operand1: ${operand1}, operand2: ${operand2}, operation: ${operationSaved}, operationflag:${operandFlag}`
+  );
+}
 
 function equalsPress() {
   doMath();
